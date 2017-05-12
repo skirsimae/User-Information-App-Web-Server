9b58683eca08c0881e5b2ce87a97e6159e49fd8f
Silva Kirsimae <skirsimae@tmk.edu.ee>
2017-05-12 16:12:56 +0200

    second test version added, part 1, route 2 and 3 completed

--- app.js
+++ app.js
@@ -20,52 +20,75 @@
 
 const express = require('express'); //express is function data type
 const fs = require ('fs');
-const app = express ( ); //app is an object
+const app = express ( ); //app is an objec
+const bodyParser = require('body-parser');
 
-const bodyParser = require('body-parser')
+// app.use('/search', bodyParser)
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+// app.use(express.urlencoded());
 
 
-app.set('views' , './src/views' ); //set -set settings ; specifying in wich folder views need to be located in. views = are the pug files, that specifies how something should look. Html and css combined. 
-app.set('view engine' , 'pug' );
+app.set('views', './src/views' ); //set -set settings ; specifying in wich folder views need to be located in. views = are the pug files, that specifies how something should look. Html and css combined. 
+app.set('view engine', 'pug' );
 
 
 //Part 0. Route 1:
 app.get('/', function (req, res) { //same as: app.get('/', function(req, res) //routes are always in the form of app.get/app.get/app.put/app.delete
-	fs.readFile('./public/users.json', function (err, data) { //data.txt is a text file //fs.readFile is a function inside an object. read line 25!
+	fs.readFile('./users.json', function (err, data) { //data.txt is a text file //fs.readFile is a function inside an object. read line 25!
 		if(err) {
 			throw err;
 		}
 
-		var info = JSON.parse(data); //parsing the data in json file and sending it to. The data in JSON file is a string and it will translate to real javascript object.
-		console.log(info); //it will console log the data. 
+		var user = JSON.parse(data); //parsing the data in json file and sending it to. The data in JSON file is a string and it will translate to real javascript object.
+//		console.log(user); it will console log the data. 
 
-		res.render('index', {users: info}); //index is called index.pug ;res.render is rendering html pug; if there is no error displays it to the webpage. stuff:data is object; 
+
+		res.render('index', {users: user}); //index is called index.pug ;res.render is rendering html pug; if there is no error displays it to the webpage. stuff:data is object; 
 	});
 });
 
 
 //Part 1. Route 2:
-app.use('/search', bodyParser)
 
 app.get('/search', function(req,res) {
+	// console.log('req.guery is: ', req.query)
 	res.render('search');
 })
 
 
 //Part 1. Route 3:
 app.post('/search', function(req,res) {
-	var input = req.body
-	
-	fs.readFile('./users.json', function(err, data) {
+	// console.log(req.body.search)
+
+	fs.readFile('./users.json', 'utf-8', function(err, data) {
     	if (err) {
         	throw err;
  		}
 
- 		var info = JSON.parse(data) 
-		console.log(req.body);
+ 	var input = req.body.name;
 
-		res.render('postsearch', {users: info});
+ 	var user = JSON.parse(data); 
+
+	for (var i = 0; i < user.length; i++) {
+		if (input === user[i].firstname) {
+			var userName = user[i].firstname;
+			var userLastname = user[i].lastname;
+			var userEmail = user[i].email;
+		}
+
+		else if (input === user[i].lastname) {
+			var userName = user[i].firstname; 
+			var userLastname = user[i].lastname; 
+			var userEmail = user[i].email;
+		}
+	}
+
+		res.render("find", {first: userName, last: userLastname, email: userEmail});
 	});
+
+
 });
 
 
